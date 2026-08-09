978f3c01f64f5a455d350264b61bc99ce94884ce
55muneeb <awanzada1289@gmail.com>
2026-08-09 10:36:48 +0500

    Add Metrics and Industries components to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 // src/pages/Home.jsx
 import React from 'react';
 import Hero from '../sections/Hero';
+import Metrics from '../sections/Metrics';
+import Industries from '../sections/Industries';
 
 export default function Home() {
   return (
     <div className="page-home">
       <Hero />
+      <Metrics />
+      <Industries />
     </div>
   );
 }
